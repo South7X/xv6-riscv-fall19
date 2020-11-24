823ca1c3f3d1fa05e384fb3402892314f2cc83d2
Xiang <heidyxianD@163.com>
2020-11-24 22:06:21 +0800

    Lab 3-1: kalloc passed

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,6 +10,7 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+int get_cpuid();
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
@@ -18,15 +19,17 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem{
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+};
+struct kmem kmems[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++)
+    initlock(&kmems[i].lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,7 +38,9 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
+  // PGROUNDUP确保空闲内存4K对齐 *
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+    //将end～PHYSTOP之间的地址空间按照页面大小切分并调用kfree()将页面从头部插入到链表kmem.freelist中进行管理 *
     kfree(p);
 }
 
@@ -47,7 +52,7 @@ void
 kfree(void *pa)
 {
   struct run *r;
-
+  int kmemid = get_cpuid();
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
@@ -55,11 +60,11 @@ kfree(void *pa)
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // Free mem to current CPU's freelist *
+  acquire(&kmems[kmemid].lock);
+  r->next = kmems[kmemid].freelist;
+  kmems[kmemid].freelist = r;
+  release(&kmems[kmemid].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,13 +75,34 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  int kmemid = get_cpuid();
+  // Allocate current cpu mem in its freelist *
+  acquire(&kmems[kmemid].lock);
+  r = kmems[kmemid].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmems[kmemid].freelist = r->next;
+  release(&kmems[kmemid].lock);
+  // Current cpu freelist is null *
+  if(!r){
+    for (int i = 0; i < NCPU; i ++) {   // Find in other cpu's freelist *
+      acquire(&kmems[i].lock);
+      r = kmems[i].freelist;
+      if(r)
+        kmems[i].freelist = r->next;
+      release(&kmems[i].lock);
 
+      if (r)  break;      // Already stolen one mem from other cpu freelist *
+    }
+  }
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+int get_cpuid()
+{
+  push_off();
+  int id = cpuid();
+  pop_off();
+  return id;
 }
